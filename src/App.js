8593ffda3954c392392dbd62e17133d78f517903
Sumit0397 <sumitmajumdar16@gmail.com>
2023-10-03 13:02:04 +0530

    Mail Box optimisations

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Authentication from './components/authentication/Authentication';
+import Compose from './components/Message/Compose';
+import Inbox from './components/Message/Inbox';
+import SentBox from './components/Message/SentBox';
+import Root from './components/Root/Root';
+import Welcome from './components/Profile/Welcome';
+import EmailMessage from './components/Message/EmailMessage';
+import { useDispatch } from 'react-redux';
+import { useEffect } from 'react';
+import { inboxFill } from './store/inboxSlice';
+import { sentboxFill } from './store/sentboxSlice';
+import SentMessage from './components/Message/SentMessage';
 
 function App() {
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (localStorage.getItem("email")) {
+      dispatch(inboxFill(localStorage.getItem("email")));
+      dispatch(sentboxFill(localStorage.getItem("email")));
+    }
+  }, [])
+
+  setInterval(() => {
+    if (localStorage.getItem("email")) {
+      dispatch(inboxFill(localStorage.getItem("email")));
+      dispatch(sentboxFill(localStorage.getItem("email")));
+      console.log("render");
+    }
+  }, 2000)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path='/' element={<Authentication />} />
+        <Route path='/profile' element={<Root />}>
+          <Route path='/profile/welcome' element={<Welcome />} />
+          <Route path='/profile/compose' element={<Compose />} />
+          <Route path='/profile/inbox' element={<Inbox />} />
+          <Route path='/profile/sentbox' element={<SentBox />} />
+          <Route path='/profile/inbox/message' element={<EmailMessage />} />
+          <Route path='/profile/sentbox/message' element={<SentMessage />} />
+        </Route>
+      </Routes>
     </div>
   );
 }
